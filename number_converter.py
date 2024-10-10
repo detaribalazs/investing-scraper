0f47edf479a27268b2eae30c3767c1d632edc145
Balázs Détári <detaribalazs@cloudera.com>
2024-10-10 14:24:53 +0200

    Reformatted ce cache

--- number_converter.py
+++ number_converter.py
@@ -1,6 +1,6 @@
 import yaml
 
-cache_file = "./cache/marketcap-2023-12.yaml"
+cache_file = "./cache/equity_common-2023-12.yaml"
 if __name__ == "__main__":
     with open(cache_file, "r") as f:
         yaml_data = dict(yaml.safe_load(f))
@@ -16,6 +16,18 @@ if __name__ == "__main__":
             shares = float(shares_str)
             shares *= 1000000
             yaml_data[key]["Shares Outstanding"] = int(shares)
+
+    if "./cache/equity_common-202" in cache_file:
+        for key, item in yaml_data.items():
+            if len(item) == 0:
+                print(f"Skipping {key}")
+                continue
+            if item["Common Equity"] in ['', "NA"] :
+                continue
+            ce = int(float(item["Common Equity"]))
+            yaml_data[key]["Common Equity"] = ce
+
+
 
     with open(cache_file, "w") as f:
         yaml.dump(yaml_data, f)
