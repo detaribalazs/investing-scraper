7b89758ba8b0f67674b15884b5de0a4faeaa3283
Balázs Détári <detaribalazs@cloudera.com>
2024-10-10 14:30:49 +0200

    Reformatted pr cache

--- number_converter.py
+++ number_converter.py
@@ -1,6 +1,6 @@
 import yaml
 
-cache_file = "./cache/equity_common-2023-12.yaml"
+cache_file = "./cache/payout_ratio-2023-12.yaml"
 if __name__ == "__main__":
     with open(cache_file, "r") as f:
         yaml_data = dict(yaml.safe_load(f))
@@ -17,7 +17,7 @@ if __name__ == "__main__":
             shares *= 1000000
             yaml_data[key]["Shares Outstanding"] = int(shares)
 
-    if "./cache/equity_common-202" in cache_file:
+    if "equity_common" in cache_file:
         for key, item in yaml_data.items():
             if len(item) == 0:
                 print(f"Skipping {key}")
@@ -26,6 +26,19 @@ if __name__ == "__main__":
                 continue
             ce = int(float(item["Common Equity"]))
             yaml_data[key]["Common Equity"] = ce
+
+    if "payout_ratio" in cache_file:
+        for key, item in yaml_data.items():
+            if len(item) == 0:
+                print(f"Skipping {key}")
+                continue
+            if item["Payout Ratio"] in ['', "NA"] :
+                continue
+            ce_str = item["Payout Ratio"]
+            if ce_str.endswith("%"):
+                ce_str = ce_str[:-1]
+                ce = float(ce_str)/100
+                yaml_data[key]["Payout Ratio"] = ce
 
 
 
